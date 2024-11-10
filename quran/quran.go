af6e182da35ea017a24e7583ac975c2ad17c09e1
asim <asim@aslam.me>
2024-11-10 07:48:22 +0000

    move markdown code

--- quran/quran.go
+++ quran/quran.go
@@ -56,3 +56,31 @@ func Load() map[string]*Chapter {
 
 	return Quran
 }
+
+func Markdown() string {
+	q := Load()
+
+	var data string
+
+	// 114 surahs
+	for i := 0; i < 114; i++ {
+		ch := q[fmt.Sprintf("%d", i+1)]
+
+		data += fmt.Sprintln()
+		data += fmt.Sprintf(`# %d`, ch.Number)
+		data += fmt.Sprintln()
+		data += fmt.Sprintln()
+		data += fmt.Sprintf(`### %s`, ch.Name)
+		data += fmt.Sprintln()
+
+		// max 286 ayahs
+		for _, verse := range ch.Verses {
+			data += fmt.Sprintln()
+			data += fmt.Sprintf(verse.Text)
+			data += fmt.Sprintln()
+		}
+
+	}
+
+	return data
+}
