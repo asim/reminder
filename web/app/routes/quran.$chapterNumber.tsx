c7ad029e1906b662e0135bedb63e08651359eb0a
Kamran Ahmed <kamranahmed.se@gmail.com>
2025-04-20 20:58:43 +0100

    Add chapter navigation

--- web/app/routes/quran.$chapterNumber.tsx
+++ web/app/routes/quran.$chapterNumber.tsx
@@ -1,5 +1,8 @@
 import type { Route } from '.react-router/types/app/routes/+types/quran.$chapterNumber';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { CircleChevronLeft, CircleChevronRight } from 'lucide-react';
+import { Link } from 'react-router';
+import { PrimaryButton } from '~/components/interface/primary-button';
 import { getChapterOptions } from '~/queries/quran';
 import { queryClient } from '~/utils/query-client';
 
@@ -17,6 +20,9 @@ export default function QuranChapter(props: Route.ComponentProps) {
     return null;
   }
 
+  const previousChapter = Number(chapterNumber) - 1;
+  const nextChapter = Number(chapterNumber) + 1;
+
   return (
     <div className='max-w-4xl mx-auto'>
       <div className='text-center mb-12'>
@@ -26,7 +32,11 @@ export default function QuranChapter(props: Route.ComponentProps) {
 
       <div className='space-y-8'>
         {data.verses.map((verse) => (
-          <div key={verse.number} className='border-b border-gray-100 pb-8'>
+          <div
+            data-chapter-verse={`${data.number}:${verse.number}`}
+            key={verse.number}
+            className='border-b border-gray-100 pb-8'
+          >
             <div className='text-3xl mb-4 text-right leading-loose font-arabic'>
               {verse.arabic}
             </div>
@@ -36,6 +46,22 @@ export default function QuranChapter(props: Route.ComponentProps) {
             </div>
           </div>
         ))}
+      </div>
+
+      <div className='flex justify-between mt-8 mb-12'>
+        <PrimaryButton asChild disabled={previousChapter <= 1}>
+          <Link to={`/quran/${previousChapter}`}>
+            <CircleChevronLeft className='size-4' />
+            Previous Chapter
+          </Link>
+        </PrimaryButton>
+
+        <PrimaryButton asChild disabled={nextChapter >= 114}>
+          <Link to={`/quran/${nextChapter}`}>
+            Next Chapter
+            <CircleChevronRight className='size-4' />
+          </Link>
+        </PrimaryButton>
       </div>
     </div>
   );
